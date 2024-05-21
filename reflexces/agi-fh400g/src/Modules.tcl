c6bac8dc11802b7994091cec1dc3097d9c1da7a7
Tomas Hak <hak@cesnet.cz>
2024-05-21 15:02:14 +0200

    CARDS: [FEATURE] add IP Tcl scripting support for agi-fh400g card

--- reflexces/agi-fh400g/src/Modules.tcl
+++ reflexces/agi-fh400g/src/Modules.tcl
@@ -15,61 +15,69 @@ set BOOT_CTRL_BASE "$OFM_PATH/../core/intel/src/comp/boot_ctrl"
 lappend COMPONENTS [list "FPGA_COMMON" $ARCHGRP_ARR(CORE_BASE)  $ARCHGRP]
 lappend COMPONENTS [list "BOOT_CTRL"   $BOOT_CTRL_BASE          "FULL"]
 
-# IP sources
-lappend MOD "$ENTITY_BASE/ip/iopll_ip.ip"
-lappend MOD "$ENTITY_BASE/ip/reset_release_ip.ip"
-lappend MOD "$ENTITY_BASE/ip/sodimm.ip"
-lappend MOD "$ENTITY_BASE/ip/sodimm_cal.ip"
-lappend MOD "$ENTITY_BASE/ip/OnBoard_DDR4.ip"
-lappend MOD "$ENTITY_BASE/ip/emif_agi027_cal.ip"
-lappend MOD "$ENTITY_BASE/ip/mailbox_client_ip.ip"
-lappend MOD "$ENTITY_BASE/ip/ftile_pll.ip"
+# IP components
+set IP_DEVICE_FAMILY "Agilex"
+set IP_DEVICE        $ARCHGRP_ARR(FPGA)
+set IP_TEMPLATE_BASE "$ARCHGRP_ARR(CORE_BASE)/src/ip/intel"
+set IP_MODIFY_BASE   "$ENTITY_BASE/ip"
 
-if {$ARCHGRP_ARR(PCIE_ENDPOINT_MODE) == 0} {
-    lappend MOD "$ENTITY_BASE/ip/rtile_pcie_1x16.ip"
+source $IP_TEMPLATE_BASE/../common.tcl
+
+set PCIE_CONF [dict create 0 "1x16" 1 "2x8"]
+set RTILE_PCIE_IP_NAME "rtile_pcie_[dict get $PCIE_CONF $ARCHGRP_ARR(PCIE_ENDPOINT_MODE)]"
+
+set ETH_CONF [dict create 400 "1x400g" 200 "2x200g" 100 [expr {$ARCHGRP_ARR(ETH_PORT_CHAN,0) == 2 ? "2x100g" : "4x100g"}] 50 "8x50g" 40 "2x40g" 25 "8x25g" 10 "8x10g"]
+set FTILE_ETH_IP_NAME "ftile_eth_[dict get $ETH_CONF $ARCHGRP_ARR(ETH_PORT_SPEED,0)]"
+
+set ETH_MR_CONF [dict create 100 "1x100g" 25 "1x25g_1x10g"]
+set FTILE_MR_ETH_IP_NAME ""
+if {$ARCHGRP_ARR(EHIP_PORT_TYPE,0) == 1} {
+    set FTILE_MR_ETH_IP_NAME "ftile_multirate_eth_[dict get $ETH_MR_CONF $ARCHGRP_ARR(ETH_PORT_SPEED,0)]"
 }
-if {$ARCHGRP_ARR(PCIE_ENDPOINT_MODE) == 1} {
-    lappend MOD "$ENTITY_BASE/ip/rtile_pcie_2x8.ip"
+
+# modify == 1 -> provide '$IP_MODIFY_BASE/<script_name>.ip.tcl' file with IP modification commands
+#                         script_path     script_name        ip_comp_name       type  modify
+lappend IP_COMPONENTS [list  "clk"     "ftile_pll"         "ftile_pll"            0      1]
+lappend IP_COMPONENTS [list  "clk"     "iopll"             "iopll_ip"             0      1]
+lappend IP_COMPONENTS [list  "mem"     "ddr4_calibration"  "emif_agi027_cal"      0      1]
+lappend IP_COMPONENTS [list  "mem"     "ddr4_calibration"  "sodimm_cal"           1      1]
+lappend IP_COMPONENTS [list  "mem"     "onboard_ddr4"      "OnBoard_DDR4"         0      1]
+lappend IP_COMPONENTS [list  "mem"     "onboard_ddr4"      "sodimm"               1      1]
+lappend IP_COMPONENTS [list  "misc"    "mailbox_client"    "mailbox_client_ip"    0      0]
+lappend IP_COMPONENTS [list  "misc"    "reset_release"     "reset_release_ip"     0      0]
+lappend IP_COMPONENTS [list  "pcie"    "rtile_pcie"        $RTILE_PCIE_IP_NAME    0      1]
+
+if {$ARCHGRP_ARR(VIRTUAL_DEBUG_ENABLE) eq true} {
+    lappend IP_COMPONENTS [list "misc"  "jtag_op"          "jtag_op_ip"           0      0]
 }
 
 if {$ARCHGRP_ARR(NET_MOD_ARCH) == "F_TILE"} {
-    if {$ARCHGRP_ARR(ETH_PORT_SPEED,0) == 400} {
-        lappend MOD "$ENTITY_BASE/ip/ftile_eth_1x400g.ip"
+    if {$ARCHGRP_ARR(EHIP_PORT_TYPE,0) == 1} {
+        lappend IP_COMPONENTS [list "eth"  "ftile_mr_eth"  $FTILE_MR_ETH_IP_NAME  0      1]
+        lappend IP_COMPONENTS [list "eth"  "dr_ctrl"       "dr_ctrl"              0      1]
+    } else {
+        lappend IP_COMPONENTS [list "eth"  "ftile_eth"     $FTILE_ETH_IP_NAME     0      1]
     }
-    if {$ARCHGRP_ARR(ETH_PORT_SPEED,0) == 200} {
-        lappend MOD "$ENTITY_BASE/ip/ftile_eth_2x200g.ip"
+}
+
+foreach ip_comp $IP_COMPONENTS {
+    set path   [lindex $ip_comp 0]
+    set script [lindex $ip_comp 1]
+    set comp   [lindex $ip_comp 2]
+    set type   [lindex $ip_comp 3]
+    set modify [lindex $ip_comp 4]
+
+    # TODO: trivial check for time-saving purpose (invent better solution in the future)
+    set ipfile $ARCHGRP_ARR(IP_BUILD_DIR)/[get_ip_filename $comp]
+    if {[file exists $ipfile]} {
+        lappend MOD $ipfile
+        continue
     }
-    if {$ARCHGRP_ARR(ETH_PORT_SPEED,0) == 100} {
-            if {$ARCHGRP_ARR(ETH_PORT_CHAN,0) == 2} {
-                    if {$ARCHGRP_ARR(EHIP_PORT_TYPE,0) == 1} {
-                        lappend MOD "$ENTITY_BASE/ip/ftile_multirate_eth_1x100g.ip"
-                        lappend MOD "$ENTITY_BASE/ip/dr_ctrl.ip"
-                    }
-                    if {$ARCHGRP_ARR(EHIP_PORT_TYPE,0) == 0} {
-                        lappend MOD "$ENTITY_BASE/ip/ftile_eth_2x100g.ip"
-                    }
-            }
-            if {$ARCHGRP_ARR(ETH_PORT_CHAN,0) == 4} {
-                lappend MOD "$ENTITY_BASE/ip/ftile_eth_4x100g.ip"
-            }
-        }
-    if {$ARCHGRP_ARR(ETH_PORT_SPEED,0) == 50} {
-        lappend MOD "$ENTITY_BASE/ip/ftile_eth_8x50g.ip"
-    }
-    if {$ARCHGRP_ARR(ETH_PORT_SPEED,0) == 40} {
-        lappend MOD "$ENTITY_BASE/ip/ftile_eth_2x40g.ip"
-    }
-    if {$ARCHGRP_ARR(ETH_PORT_SPEED,0) == 25} {
-        if {$ARCHGRP_ARR(EHIP_PORT_TYPE,0) == 0} {
-            lappend MOD "$ENTITY_BASE/ip/ftile_eth_8x25g.ip"
-        }
-        if {$ARCHGRP_ARR(EHIP_PORT_TYPE,0) == 1} {
-            lappend MOD "$ENTITY_BASE/ip/ftile_multirate_eth_1x25g_1x10g.ip"
-            lappend MOD "$ENTITY_BASE/ip/dr_ctrl.ip"
-        }
-    }
-    if {$ARCHGRP_ARR(ETH_PORT_SPEED,0) == 10} {
-        lappend MOD "$ENTITY_BASE/ip/ftile_eth_8x10g.ip"
+
+    set params_l [concat $ARCHGRP "IP_DEVICE_FAMILY" $IP_DEVICE_FAMILY "IP_DEVICE" $IP_DEVICE "IP_COMP_NAME" $comp "IP_COMP_TYPE" $type]
+    lappend MOD [list "$IP_TEMPLATE_BASE/$path/$script.ip.tcl" TYPE "QUARTUS_TCL" PHASE { "ADD_FILES" "IP_TEMPLATE_GEN" } IP_BUILD_DIR $ARCHGRP_ARR(IP_BUILD_DIR) VARS [list IP_PARAMS_L $params_l]]
+    if {$modify == 1} {
+        lappend MOD [list "$IP_MODIFY_BASE/$script.ip.tcl"     TYPE "QUARTUS_TCL" PHASE { "ADD_FILES" "IP_MODIFY"       } IP_BUILD_DIR $ARCHGRP_ARR(IP_BUILD_DIR) VARS [list IP_PARAMS_L $params_l]]
     }
 }
 
