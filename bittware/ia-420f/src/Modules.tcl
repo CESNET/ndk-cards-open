12c5b7153d93569f5ce79f97760dcb4086b0ae44
Tomas Hak <hak@cesnet.cz>
2024-05-21 14:59:27 +0200

    CARDS: [FEATURE] add IP Tcl scripting support for ia-420f card

--- bittware/ia-420f/src/Modules.tcl
+++ bittware/ia-420f/src/Modules.tcl
@@ -1,6 +1,7 @@
 # Modules.tcl: script to compile IA-420F card
 # Copyright (C) 2022 CESNET z. s. p. o.
 # Author(s): Daniel Kříž <xkrizd01@vutbr.cz.cz>
+#            Tomas Hak <hak@cesnet.cz>
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -15,30 +16,56 @@ lappend COMPONENTS [list "I2C IO EXPANDER"   $IOEXP_BASE               "FULL"]
 lappend COMPONENTS [list "OPEN_LOOP"         $ASYNC_BASE/open_loop     "FULL"]
 lappend COMPONENTS [list "RESET"             $ASYNC_BASE/reset         "FULL"]
 
-# IP sources
-lappend MOD "$ENTITY_BASE/ip/iopll_ip.ip"
-lappend MOD "$ENTITY_BASE/ip/reset_release_ip.ip"
-lappend MOD "$ENTITY_BASE/ip/mailbox_client_ip.ip"
-lappend MOD "$ENTITY_BASE/ip/ddr4_calibration.ip"
-lappend MOD "$ENTITY_BASE/ip/onboard_ddr4_0.ip"
-lappend MOD "$ENTITY_BASE/ip/onboard_ddr4_1.ip"
+# IP components
+set IP_DEVICE_FAMILY  "Agilex"
+set IP_DEVICE         $ARCHGRP_ARR(FPGA)
+set IP_TEMPLATE_BASE  "$ARCHGRP_ARR(CORE_BASE)/src/ip/intel"
+set IP_MODIFY_BASE    "$ENTITY_BASE/ip"
 
-if {$ARCHGRP_ARR(PCIE_ENDPOINT_MODE) == 0} {
-    lappend MOD "$ENTITY_BASE/ip/ptile_pcie_1x16.ip"
-}
-if {$ARCHGRP_ARR(PCIE_ENDPOINT_MODE) == 1} {
-    lappend MOD "$ENTITY_BASE/ip/ptile_pcie_2x8.ip"
+source $IP_TEMPLATE_BASE/../common.tcl
+
+set PCIE_CONF [dict create 0 "1x16" 1 "2x8"]
+set PTILE_PCIE_IP_NAME "ptile_pcie_[dict get $PCIE_CONF $ARCHGRP_ARR(PCIE_ENDPOINT_MODE)]"
+
+set ETH_CONF [dict create 100 "1x100g" 25 "4x25g" 10 "4x10g"]
+set ETILE_ETH_IP_NAME "etile_eth_[dict get $ETH_CONF $ARCHGRP_ARR(ETH_PORT_SPEED,0)]"
+
+# modify == 1 -> provide '$IP_MODIFY_BASE/<script_name>.ip.tcl' file with IP modification commands
+#                         script_path    script_name       ip_comp_name     type  modify
+lappend IP_COMPONENTS [list  "clk"    "iopll"            "iopll_ip"           0      1]
+lappend IP_COMPONENTS [list  "mem"    "ddr4_calibration" "ddr4_calibration"   0      0]
+lappend IP_COMPONENTS [list  "mem"    "onboard_ddr4"     "onboard_ddr4_0"     0      1]
+lappend IP_COMPONENTS [list  "mem"    "onboard_ddr4"     "onboard_ddr4_1"     1      1]
+lappend IP_COMPONENTS [list  "misc"   "mailbox_client"   "mailbox_client_ip"  0      0]
+lappend IP_COMPONENTS [list  "misc"   "reset_release"    "reset_release_ip"   0      0]
+lappend IP_COMPONENTS [list  "pcie"   "ptile_pcie"       $PTILE_PCIE_IP_NAME  0      1]
+
+if {$ARCHGRP_ARR(VIRTUAL_DEBUG_ENABLE) eq true} {
+    lappend IP_COMPONENTS [list "misc"  "jtag_op"        "jtag_op_ip"         0      0]
 }
 
 if {$ARCHGRP_ARR(NET_MOD_ARCH) == "E_TILE"} {
-    if {$ARCHGRP_ARR(ETH_PORT_SPEED,0) == 100} {
-        lappend MOD "$ENTITY_BASE/ip/etile_eth_1x100g.ip"
+    lappend IP_COMPONENTS [list "eth"   "etile_eth"      $ETILE_ETH_IP_NAME   0      1]
+}
+
+foreach ip_comp $IP_COMPONENTS {
+    set path   [lindex $ip_comp 0]
+    set script [lindex $ip_comp 1]
+    set comp   [lindex $ip_comp 2]
+    set type   [lindex $ip_comp 3]
+    set modify [lindex $ip_comp 4]
+
+    # TODO: trivial check for time-saving purpose (invent better solution in the future)
+    set ipfile $ARCHGRP_ARR(IP_BUILD_DIR)/[get_ip_filename $comp]
+    if {[file exists $ipfile]} {
+        lappend MOD $ipfile
+        continue
     }
-    if {$ARCHGRP_ARR(ETH_PORT_SPEED,0) == 25} {
-        lappend MOD "$ENTITY_BASE/ip/etile_eth_4x25g.ip"
-    }
-    if {$ARCHGRP_ARR(ETH_PORT_SPEED,0) == 10} {
-        lappend MOD "$ENTITY_BASE/ip/etile_eth_4x10g.ip"
+
+    set params_l [concat $ARCHGRP "IP_DEVICE_FAMILY" $IP_DEVICE_FAMILY "IP_DEVICE" $IP_DEVICE "IP_COMP_NAME" $comp "IP_COMP_TYPE" $type]
+    lappend MOD [list "$IP_TEMPLATE_BASE/$path/$script.ip.tcl" TYPE "QUARTUS_TCL" PHASE { "ADD_FILES" "IP_TEMPLATE_GEN" } IP_BUILD_DIR $ARCHGRP_ARR(IP_BUILD_DIR) VARS [list IP_PARAMS_L $params_l]]
+    if {$modify == 1} {
+        lappend MOD [list "$IP_MODIFY_BASE/$script.ip.tcl"     TYPE "QUARTUS_TCL" PHASE { "ADD_FILES" "IP_MODIFY"       } IP_BUILD_DIR $ARCHGRP_ARR(IP_BUILD_DIR) VARS [list IP_PARAMS_L $params_l]]
     }
 }
 
