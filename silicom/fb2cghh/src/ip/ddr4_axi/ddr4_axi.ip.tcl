4c95215c8481790139ec31ff6b6c0107ae3da531
Tomas Hak <hak@cesnet.cz>
2024-05-29 11:28:10 +0200

    CARDS: [MAINTENANCE] reduce IP properties setting (already set as default values)

--- silicom/fb2cghh/src/ip/ddr4_axi/ddr4_axi.ip.tcl
+++ silicom/fb2cghh/src/ip/ddr4_axi/ddr4_axi.ip.tcl
@@ -13,26 +13,15 @@ set IP [get_ips $IP_COMP_NAME]
 
 set_property -dict [list \
     CONFIG.C0.BANK_GROUP_WIDTH {1} \
-    CONFIG.C0.CS_WIDTH {1} \
     CONFIG.C0.DDR4_AxiDataWidth {512} \
     CONFIG.C0.DDR4_AxiSelection {true} \
     CONFIG.C0.DDR4_CLKFBOUT_MULT {5} \
-    CONFIG.C0.DDR4_CLKOUT0_DIVIDE {4} \
     CONFIG.C0.DDR4_CasLatency {22} \
-    CONFIG.C0.DDR4_CasWriteLatency {14} \
-    CONFIG.C0.DDR4_Clamshell {false} \
     CONFIG.C0.DDR4_CustomParts [subst $PARAMS(IP_EXT_BASE)/fb2cghh_onboard_ddr4.csv] \
-    CONFIG.C0.DDR4_DIVCLK_DIVIDE {1} \
     CONFIG.C0.DDR4_DataMask {NO_DM_NO_DBI} \
     CONFIG.C0.DDR4_DataWidth {72} \
     CONFIG.C0.DDR4_Ecc {true} \
     CONFIG.C0.DDR4_InputClockPeriod {3750} \
-    CONFIG.C0.DDR4_Mem_Add_Map {ROW_COLUMN_BANK} \
     CONFIG.C0.DDR4_MemoryPart {sdk_DDR4_2666} \
-    CONFIG.C0.DDR4_MemoryType {Components} \
-    CONFIG.C0.DDR4_Specify_MandD {false} \
-    CONFIG.C0.DDR4_TimePeriod {750} \
     CONFIG.C0.DDR4_isCustom {true} \
-    CONFIG.C0.DDR4_nCK_TXPR {5} \
-    CONFIG.System_Clock {Differential} \
 ] $IP
--- silicom/fb2cghh/src/ip/pcie4_uscale_plus/pcie4_uscale_plus.ip.tcl
+++ silicom/fb2cghh/src/ip/pcie4_uscale_plus/pcie4_uscale_plus.ip.tcl
@@ -27,47 +27,14 @@ set_property -dict [list \
     CONFIG.PF0_SUBSYSTEM_ID {00d2} \
     CONFIG.PF0_SUBSYSTEM_VENDOR_ID {1c2c} \
     CONFIG.PF0_Use_Class_Code_Lookup_Assistant {true} \
-    CONFIG.PF1_MSIX_CAP_PBA_BIR {BAR_1:0} \
-    CONFIG.PF1_MSIX_CAP_TABLE_BIR {BAR_1:0} \
-    CONFIG.PF1_MSIX_CAP_TABLE_SIZE {0} \
-    CONFIG.PF1_SUBSYSTEM_VENDOR_ID {1c2c} \
-    CONFIG.PF2_MSIX_CAP_PBA_BIR {BAR_1:0} \
-    CONFIG.PF2_MSIX_CAP_TABLE_BIR {BAR_1:0} \
-    CONFIG.PF2_MSIX_CAP_TABLE_SIZE {0} \
-    CONFIG.PF2_SUBSYSTEM_VENDOR_ID {1c2c} \
-    CONFIG.PF3_MSIX_CAP_PBA_BIR {BAR_1:0} \
-    CONFIG.PF3_MSIX_CAP_TABLE_BIR {BAR_1:0} \
-    CONFIG.PF3_MSIX_CAP_TABLE_SIZE {0} \
-    CONFIG.PF3_SUBSYSTEM_VENDOR_ID {1c2c} \
-    CONFIG.PL_DISABLE_LANE_REVERSAL {TRUE} \
     CONFIG.PL_LINK_CAP_MAX_LINK_SPEED {8.0_GT/s} \
     CONFIG.SRIOV_CAP_ENABLE [subst $SRIOV_CAP_ENABLE] \
     CONFIG.SRIOV_FIRST_VF_OFFSET [subst $SRIOV_FIRST_VF_OFFSET] \
-    CONFIG.VFG0_MSIX_CAP_PBA_OFFSET {0} \
-    CONFIG.VFG0_MSIX_CAP_TABLE_OFFSET {0} \
-    CONFIG.VFG1_MSIX_CAP_PBA_OFFSET {0} \
-    CONFIG.VFG1_MSIX_CAP_TABLE_OFFSET {0} \
-    CONFIG.VFG2_MSIX_CAP_PBA_OFFSET {0} \
-    CONFIG.VFG2_MSIX_CAP_TABLE_OFFSET {0} \
-    CONFIG.VFG3_MSIX_CAP_PBA_OFFSET {0} \
-    CONFIG.VFG3_MSIX_CAP_TABLE_OFFSET {0} \
     CONFIG.axisten_freq {250} \
     CONFIG.axisten_if_enable_client_tag {true} \
-    CONFIG.axisten_if_enable_msg_route {00000} \
-    CONFIG.cfg_ctl_if {true} \
-    CONFIG.cfg_fc_if {true} \
-    CONFIG.cfg_mgmt_if {true} \
-    CONFIG.cfg_pm_if {true} \
-    CONFIG.cfg_status_if {true} \
-    CONFIG.cfg_tx_msg_if {true} \
     CONFIG.coreclk_freq {500} \
-    CONFIG.en_pcie_conf {false} \
     CONFIG.ext_pcie_cfg_space_enabled {true} \
-    CONFIG.gen_x0y1 {false} \
-    CONFIG.gen_x1y0 {true} \
-    CONFIG.gen_x1y4 {false} \
     CONFIG.mode_selection {Advanced} \
-    CONFIG.pcie_blk_locn {X1Y0} \
     CONFIG.pf0_bar0_64bit {true} \
     CONFIG.pf0_bar0_scale {Megabytes} \
     CONFIG.pf0_bar0_size {64} \
@@ -81,31 +48,10 @@ set_property -dict [list \
     CONFIG.pf0_sriov_bar0_64bit {true} \
     CONFIG.pf0_sriov_bar0_size {64} \
     CONFIG.pf0_sub_class_interface_menu {Ethernet_controller} \
-    CONFIG.pf1_bar0_64bit {true} \
-    CONFIG.pf1_bar0_scale {Megabytes} \
-    CONFIG.pf1_bar0_size {64} \
     CONFIG.pf1_rbar_cap_bar0 {0xffffffffffff} \
-    CONFIG.pf1_sriov_bar0_64bit {true} \
-    CONFIG.pf1_sriov_bar0_size {64} \
-    CONFIG.pf1_vendor_id {1c2c} \
-    CONFIG.pf2_bar0_64bit {true} \
-    CONFIG.pf2_bar0_scale {Megabytes} \
-    CONFIG.pf2_bar0_size {64} \
     CONFIG.pf2_rbar_cap_bar0 {0xffffffffffff} \
-    CONFIG.pf2_sriov_bar0_64bit {true} \
-    CONFIG.pf2_sriov_bar0_size {64} \
-    CONFIG.pf2_vendor_id {1c2c} \
-    CONFIG.pf3_bar0_64bit {true} \
-    CONFIG.pf3_bar0_scale {Megabytes} \
-    CONFIG.pf3_bar0_size {64} \
     CONFIG.pf3_rbar_cap_bar0 {0xffffffffffff} \
-    CONFIG.pf3_sriov_bar0_64bit {true} \
-    CONFIG.pf3_sriov_bar0_size {64} \
-    CONFIG.pf3_vendor_id {1c2c} \
     CONFIG.plltype {QPLL1} \
-    CONFIG.rcv_msg_if {true} \
-    CONFIG.select_quad {GTH_Quad_227} \
-    CONFIG.tx_fc_if {true} \
     CONFIG.type1_membase_memlimit_enable {Disabled} \
     CONFIG.type1_prefetchable_membase_memlimit {Disabled} \
     CONFIG.vendor_id {1c2c} \
@@ -114,8 +60,6 @@ set_property -dict [list \
 # x8_low_latency properties
 if {$PARAMS(PCIE_ENDPOINT_MODE) == 2} {
     set_property -dict [list \
-        CONFIG.AXISTEN_IF_EXT_512_RC_4TLP_STRADDLE {false} \
-        CONFIG.AXISTEN_IF_EXT_512_RQ_STRADDLE {false} \
         CONFIG.PF0_INTERRUPT_PIN {INTA} \
         CONFIG.PF0_MSIX_CAP_PBA_OFFSET {00000050} \
         CONFIG.PF0_MSIX_CAP_TABLE_OFFSET {00000040} \
@@ -128,28 +72,12 @@ if {$PARAMS(PCIE_ENDPOINT_MODE) == 2} {
         CONFIG.PL_LINK_CAP_MAX_LINK_WIDTH {X8} \
         CONFIG.axisten_if_width {256_bit} \
         CONFIG.en_gt_selection {true} \
-        CONFIG.msix_type {HARD} \
         CONFIG.pf0_ari_enabled {true} \
         CONFIG.pf0_bar2_64bit {true} \
         CONFIG.pf0_bar2_enabled {true} \
         CONFIG.pf0_bar2_scale {Megabytes} \
         CONFIG.pf0_bar2_size {64} \
         CONFIG.pf0_bar2_type {Memory} \
-        CONFIG.pf1_bar2_64bit {true} \
-        CONFIG.pf1_bar2_enabled {true} \
-        CONFIG.pf1_bar2_scale {Megabytes} \
-        CONFIG.pf1_bar2_size {64} \
-        CONFIG.pf1_bar2_type {Memory} \
-        CONFIG.pf2_bar2_64bit {true} \
-        CONFIG.pf2_bar2_enabled {true} \
-        CONFIG.pf2_bar2_scale {Megabytes} \
-        CONFIG.pf2_bar2_size {64} \
-        CONFIG.pf2_bar2_type {Memory} \
-        CONFIG.pf3_bar2_64bit {true} \
-        CONFIG.pf3_bar2_enabled {true} \
-        CONFIG.pf3_bar2_scale {Megabytes} \
-        CONFIG.pf3_bar2_size {64} \
-        CONFIG.pf3_bar2_type {Memory} \
     ] $IP
 
 # x16 properties
@@ -157,19 +85,14 @@ if {$PARAMS(PCIE_ENDPOINT_MODE) == 2} {
     set_property -dict [list \
         CONFIG.AXISTEN_IF_EXT_512_RC_4TLP_STRADDLE {true} \
         CONFIG.AXISTEN_IF_EXT_512_RQ_STRADDLE {true} \
-        CONFIG.PF0_INTERRUPT_PIN {NONE} \
         CONFIG.PF0_MSIX_CAP_PBA_OFFSET {01408000} \
         CONFIG.PF0_MSIX_CAP_TABLE_OFFSET {01400000} \
         CONFIG.PF0_MSIX_CAP_TABLE_SIZE {1ff} \
-        CONFIG.PF0_SRIOV_CAP_INITIAL_VF {0} \
-        CONFIG.PF0_SRIOV_FIRST_VF_OFFSET {0} \
         CONFIG.PF0_DEVICE_ID {00d2} \
         CONFIG.PF2_DEVICE_ID {943F} \
         CONFIG.PF3_DEVICE_ID {963F} \
         CONFIG.PL_LINK_CAP_MAX_LINK_WIDTH {X16} \
         CONFIG.axisten_if_width {512_bit} \
-        CONFIG.en_gt_selection {false} \
         CONFIG.msix_type {SOFT} \
-        CONFIG.pf0_ari_enabled {false} \
     ] $IP
 }
