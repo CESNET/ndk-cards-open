ab76e1f89a382a7761e8cf3d29c08a0b5c46d90e
Jakub Cabal <cabal@cesnet.cz>
2024-08-16 14:05:01 +0200

    Alveo-U200: [MAINTENACE] Set PCIE_CONF=1xGen3x16 as default

--- amd/alveo-u200/config/card_conf.tcl
+++ amd/alveo-u200/config/card_conf.tcl
@@ -43,7 +43,7 @@ set ETH_PORT_LANES(1) 4
 # ------------------------------------------------------------------------------
 
 # Set default PCIe configuration
-set PCIE_CONF "1xGen3x8LL"
+set PCIE_CONF "1xGen3x16"
 if { [info exist env(PCIE_CONF)] } {
     set PCIE_CONF $env(PCIE_CONF)
 }
